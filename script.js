98b8f748da45a87fbe44e690806f4ce060a322f1
jazzrail <jazzrail@hotmail.com>
2022-03-17 09:38:32 +0000

    Added fetch api for quotes

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello Windy');
+// Get quotes form API
+let apiQuotes = [];
+
+// Show new quote
+
+function newQuote() {
+	// pick a random quote from api quotes array
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+	console.log(quote);
+}
+
+async function getQuotes() {
+	const apiUrl = 'https://type.fit/api/quotes';
+	try {
+		const response = await fetch(apiUrl);
+		apiQuotes = await response.json();
+		newQuote();
+	} catch (error) {
+		// Catch error here
+	}
+}
+
+// On load
+getQuotes();
